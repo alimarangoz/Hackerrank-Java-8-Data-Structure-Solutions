45557c02ae7e81d3a2c03b3aa9e3971c0b0ea7e8
alimarangoz <57812346+alimarangoz@users.noreply.github.com>
2021-04-18 22:31:51 +0300

    Add files via upload

---
+++ secondSmallestValue.java
@@ -0,0 +1,41 @@
+package Schedule;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class secondSmallestValue {
+        static void secondSmallest(int arr[]) {
+        int smallest = Integer.MAX_VALUE;
+        int secondSmallest = Integer.MAX_VALUE;
+        int arr_size = arr.length;
+
+
+        for (int i = 0; i < arr_size ; i ++) {
+
+            if (arr[i] < smallest)
+            {
+                secondSmallest = smallest;
+                smallest = arr[i];
+            }
+            
+            else if (arr[i] < secondSmallest && arr[i] != smallest)
+                secondSmallest = arr[i];
+        }
+
+        if (secondSmallest == Integer.MAX_VALUE)
+            System.out.println("There is no secondSmallest smallest element");
+        else
+            System.out.println(secondSmallest);
+    }
+
+    public static void main (String[] args)
+    {
+        int arr[] = {13,13,15,14,15};
+        System.out.println(secondSmallest(arr););
+    }
+
+}
+
